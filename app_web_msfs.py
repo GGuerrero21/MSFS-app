97a1d8963e877a5c6bad75ad0de5c7be74218174
Gonzalo Guerrero <91702444+GGuerrero21@users.noreply.github.com>
2026-04-08 18:46:18 -0400

    Update app_web_msfs.py

--- app_web_msfs.py
+++ app_web_msfs.py
@@ -55,7 +55,7 @@ AEROLINEAS_BASE = [
 MODELOS_AVION = [
     "Avro RJ/BAe 146", "ATR 42-600", "ATR 72-600", "Airbus A319", "Airbus A320", "Airbus A320 Neo", "Airbus A321", "Airbus A321 Neo", 
     "Airbus A330-900", "Airbus A340-600", "Airbus A350-900", "Airbus A350-1000", "Airbus A380-800", 
-    "Boeing 737-600", "Boeing 737-700", "Boeing 737-800", "Boeing 737-900", "Boeing 747-8", 
+    "Boeing 737-600", "Boeing 737-700", "Boeing 737-800", "Boeing 737 MAX 8", "Boeing 737-900", "Boeing 747-8", 
     "Boeing 777-200", "Boeing 777-200LR", "Boeing 777-300ER", "Boeing 777F", "Boeing 787-8", 
     "Boeing 787-9", "Boeing 787-10", "Embraer E170", "Embraer E190", "Embraer E195"
 ]
